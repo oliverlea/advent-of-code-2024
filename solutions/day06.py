0eb8efe4c2373b04544b69d98b8d71aaea5e49ba
Oliver Lea <oliverglea@gmail.com>
2024-12-06 13:05:19 +0000

    Day 6 optimizations, part 2 3->1 seconds

--- solutions/day06.py
+++ solutions/day06.py
@@ -35,15 +35,24 @@ def move(start: Tuple[int, int], dir: Tuple[int, int], lines: List[str], additio
         result.append(cur)
     return result
 
-def find_path(guard_start: Tuple[int, int], dir: str, lines: List[str]) -> List[Tuple[int, int]]:
+def find_path(guard_start: Tuple[int, int], dir: str, lines: List[str]) -> List[Tuple[Tuple[int, int], str]]:
+    """
+    Return type is (location, dir), where dir is the direction the guard is facing after finding an obstacle.
+    For example, for any move in which the guard moves forward freely the direction will be the direction
+    of momentum. If the guard reaches an obstacle, the direction will be 90o right from the original direction
+    of momentum.
+    """
     cur = guard_start
-    path: List[Tuple[int, int]] = []
+    path: List[Tuple[Tuple[int, int], str]] = []
     while within_bounds(cur, lines):
         moved = move(cur, DIR_MASK[dir], lines)
         if moved:
             for loc in moved:
                 if within_bounds(loc, lines):
-                    path.append(loc)
+                    if loc == moved[-1]:
+                        path.append((loc, DIR_NEXT[dir]))
+                    else:
+                        path.append((loc, dir))
             cur = moved[-1]
         dir = DIR_NEXT[dir]
     return path
@@ -51,7 +60,7 @@ def find_path(guard_start: Tuple[int, int], dir: str, lines: List[str]) -> List[
 def part1(lines: List[str]) -> int:
     guard = find_guard(lines)
     dir = lines[guard[1]][guard[0]]
-    return len(set(find_path(guard, dir, lines)))
+    return len(set(x[0] for x in find_path(guard, dir, lines)))
 
 def loops(guard_start: Tuple[int, int], dir: str, new_obstacle: Tuple[int, int], lines: List[str]) -> bool:
     cur = guard_start
@@ -74,9 +83,21 @@ def part2(lines: List[str]) -> int:
     dir = lines[guard[1]][guard[0]]
     path = find_path(guard, dir, lines)[1:]
     spath = set()
-    path = [x for x in path if not (x in spath or spath.add(x) or x == guard)]
-    loop_count = [new_obs for new_obs in path if loops(guard, dir, new_obs, lines)]
-    return len(loop_count)
+    path = [x for x in path if not (x[0] in spath or spath.add(x[0]) or x[0] == guard)]
+    # return sum(1 for new_obs in path if loops(guard, dir, new_obs[0], lines))
+    
+    # optimize slightly by only searching for loops from the original path and skipping
+    # all of the movement up until the preceding guards location on the original path
+    prev_guard = guard
+    prev_guard_dir = dir
+    result = 0
+    for loc, new_dir in path:
+        if loops(prev_guard, prev_guard_dir, loc, lines):
+            result += 1
+        prev_guard = loc
+        prev_guard_dir = new_dir
+
+    return result
 
 if __name__ == '__main__':
     lines = [x.strip() for x in read_file('day06.txt')]
