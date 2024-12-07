f56615763b85f47be990d0b8a69912015903f40c
Oliver Lea <oliverglea@gmail.com>
2024-12-07 08:09:58 +0000

    Day 7 speedup

--- solutions/day07.py
+++ solutions/day07.py
@@ -5,7 +5,7 @@ from utils import read_file
 OPS = [
     lambda x, y: x + y,
     lambda x, y: x * y,
-    lambda x, y: int(str(x) + str(y)),
+    lambda x, y: int(f"{x}{y}")
 ]
 
 def parse(line: str) -> Tuple[int, List[int]]:
@@ -14,11 +14,13 @@ def parse(line: str) -> Tuple[int, List[int]]:
     return int(target), values
 
 def solve(xs: List[Tuple[int, List[int]]], ops) -> int:
-    def solvable(target: int, inputs: List[int], cur_sum: int) -> bool:
+    def solvable(target: int, inputs: List[int], cur_sum: int) -> int:
         if not inputs:
-            return cur_sum == target
-        n = inputs[0]
-        return any(solvable(target, inputs[1:], op(cur_sum, n)) for op in ops)
+            return target if cur_sum == target else 0
+        for op in ops:
+            if (r := op(cur_sum, inputs[0])) <= target and solvable(target, inputs[1:], r):
+                return target
+        return 0
 
     return sum(target for target, inputs in xs if solvable(target, inputs, 0))
 
